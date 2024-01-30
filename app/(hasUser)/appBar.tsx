e7c05c449a38facf46536cc61a698a965cfee8f5
Henry Yu <henrytsyu@gmail.com>
2024-01-30 14:51:18 +0000

    feat: display user name in app bar

--- app/(hasUser)/appBar.tsx
+++ app/(hasUser)/appBar.tsx
@@ -9,14 +9,34 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Card, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { createClient } from "@/utils/supabase/server";
 import { LogOut, Settings } from "lucide-react";
+import { cookies } from "next/headers";
 import Link from "next/link";
 
 export default async function AppBar() {
+  const supabase = createClient(cookies());
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const { data } = await supabase
+    .from("users")
+    .select("display_name")
+    .eq("id", user!.id)
+    .single();
+  const { display_name } = data!;
+
   return (
     <Card className="p-4 m-4 mb-0">
       <CardTitle className="flex justify-between">
-        <Link href="/">Event Run</Link>
+        <div className="flex space-x-4">
+          <Link href="/">Event Run</Link>
+          <Separator orientation="vertical" />
+          <span>{display_name ?? user!.email ?? "Participant"}</span>
+        </div>
         <div className="flex space-x-4">
           <Link href="/settings">
             <Settings />
