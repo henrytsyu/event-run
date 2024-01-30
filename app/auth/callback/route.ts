409492cc9174a786a7b9a038483d530a357bb526
Henry Yu <henrytsyu@gmail.com>
2024-01-30 12:05:31 +0000

    refactor: use NextRequest for api routes

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,12 +1,13 @@
 import { createClient } from "@/utils/supabase/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import { NextURL } from "next/dist/server/web/next-url";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest): Promise<NextResponse> {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the Auth Helpers package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
-  const requestUrl = new URL(request.url);
+  const requestUrl: NextURL = request.nextUrl;
   const code = requestUrl.searchParams.get("code");
 
   if (code) {
