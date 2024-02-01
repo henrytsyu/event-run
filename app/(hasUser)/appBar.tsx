94789057cb17b79c55ce28331b378f6f47b36493
Henry Yu <henrytsyu@gmail.com>
2024-02-01 13:05:48 +0000

    fix: remove user name display in app bar to prevent screen overflow

--- app/(hasUser)/appBar.tsx
+++ app/(hasUser)/appBar.tsx
@@ -9,34 +9,14 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Card, CardTitle } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
-import { createClient } from "@/utils/supabase/server";
 import { LogOut, Settings } from "lucide-react";
-import { cookies } from "next/headers";
 import Link from "next/link";
 
 export default async function AppBar() {
-  const supabase = createClient(cookies());
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  const { data } = await supabase
-    .from("users")
-    .select("display_name")
-    .eq("id", user!.id)
-    .single();
-  const { display_name } = data!;
-
   return (
     <Card className="p-4 m-4 mb-0">
       <CardTitle className="flex justify-between">
-        <div className="flex space-x-4">
-          <Link href="/">Event Run</Link>
-          <Separator orientation="vertical" />
-          <span>{display_name ?? user!.email ?? "Participant"}</span>
-        </div>
+        <Link href="/">Event Run</Link>
         <div className="flex space-x-4">
           <Link href="/settings">
             <Settings />
@@ -53,9 +33,9 @@ export default async function AppBar() {
               </AlertDialogHeader>
               <AlertDialogFooter>
                 <AlertDialogCancel>Cancel</AlertDialogCancel>
-                <Link href="/logout">
-                  <AlertDialogAction>Logout</AlertDialogAction>
-                </Link>
+                <AlertDialogAction asChild>
+                  <Link href="/logout">Logout</Link>
+                </AlertDialogAction>
               </AlertDialogFooter>
             </AlertDialogContent>
           </AlertDialog>
