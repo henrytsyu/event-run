c38b06ca0be0cd1b883ca000c5d720a8322e1176
Henry Yu <henrytsyu@gmail.com>
2024-02-02 17:21:48 +0000

    feat: card titles in organiser dashboard

--- app/(hasUser)/organiserPage.tsx
+++ app/(hasUser)/organiserPage.tsx
@@ -1,3 +1,31 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+
 export default async function OrganiserHome() {
-  return <div className="p-4 flex flex-col space-y-4"></div>;
+  const supabase = createClient(cookies());
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return (
+    <div className="p-4 flex flex-col space-y-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>My Live Events</CardTitle>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>My Draft Events</CardTitle>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Create New Event</CardTitle>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 }
