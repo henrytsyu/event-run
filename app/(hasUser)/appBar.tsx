35c1abea2eb0d0bd5e7b285fe9a97d99080a6c47
Henry Yu <henrytsyu@gmail.com>
2024-02-03 00:44:55 +0000

    fix: card paddings in app bar

--- app/(hasUser)/appBar.tsx
+++ app/(hasUser)/appBar.tsx
@@ -8,39 +8,41 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-import { Card, CardTitle } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { LogOut, Settings } from "lucide-react";
 import Link from "next/link";
 
 export default async function AppBar() {
   return (
-    <Card className="p-4 m-4 mb-0">
-      <CardTitle className="flex justify-between">
-        <Link href="/">Event Run</Link>
-        <div className="flex space-x-4">
-          <Link href="/settings">
-            <Settings />
-          </Link>
-          <AlertDialog>
-            <AlertDialogTrigger>
-              <LogOut />
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>
-                  Are you sure you want to logout?
-                </AlertDialogTitle>
-              </AlertDialogHeader>
-              <AlertDialogFooter>
-                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                <AlertDialogAction asChild>
-                  <Link href="/logout">Logout</Link>
-                </AlertDialogAction>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
-        </div>
-      </CardTitle>
+    <Card className="m-4 mb-0">
+      <CardHeader>
+        <CardTitle className="flex justify-between">
+          <Link href="/">Event Run</Link>
+          <div className="flex space-x-4">
+            <Link href="/settings">
+              <Settings />
+            </Link>
+            <AlertDialog>
+              <AlertDialogTrigger>
+                <LogOut />
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>
+                    Are you sure you want to logout?
+                  </AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction asChild>
+                    <Link href="/logout">Logout</Link>
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+        </CardTitle>
+      </CardHeader>
     </Card>
   );
 }
