5618d09e0869fa0421dce37dd01f6277777a14d6
Henry Yu <henrytsyu@gmail.com>
2024-02-08 11:42:17 +0000

    hotfix: hide events when record not found

--- app/(hasUser)/organiserPage.tsx
+++ app/(hasUser)/organiserPage.tsx
@@ -37,20 +37,22 @@ export default async function OrganiserHome() {
           <CardTitle>My Live Events</CardTitle>
         </CardHeader>
         <CardContent className="flex flex-col space-y-4">
-          {liveEvents?.map((record, i) => {
-            return (
-              <Link href={`/session/${record.id}`} key={i}>
-                <Card>
-                  <CardHeader>
-                    <CardTitle>{record.events!.name}</CardTitle>
-                  </CardHeader>
-                  <CardContent>
-                    {new Date(record.created_at).toDateString()}
-                  </CardContent>
-                </Card>
-              </Link>
-            );
-          })}
+          {liveEvents
+            ?.filter((record) => record.events)
+            .map((record, i) => {
+              return (
+                <Link href={`/session/${record.id}`} key={i}>
+                  <Card>
+                    <CardHeader>
+                      <CardTitle>{record.events?.name}</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      {new Date(record.created_at).toDateString()}
+                    </CardContent>
+                  </Card>
+                </Link>
+              );
+            })}
         </CardContent>
       </Card>
       <Card>
